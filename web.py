975eec8fe73cfc6774b6787bc7f97c3412876d7f
AltomarGabriel <69995296+AltomarGabriel@users.noreply.github.com>
2020-10-01 21:14:55 -0300

    Update web.py

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Gabriel Altomar"
+    return "Gabriel Altomar!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
